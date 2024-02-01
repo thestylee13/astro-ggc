036ae4e6d23fa4b51c4e59e21bcf3e4933953af0
thestylee13 <74990921+thestylee13@users.noreply.github.com>
2024-02-01 13:54:46 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+// Example for HomePage.jsx
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the home page!</p>
+  </div>
+);
+
+export default HomePage;
